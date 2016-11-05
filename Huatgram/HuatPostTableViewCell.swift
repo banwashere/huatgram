fe21f24481e127ee000c82b6c519f4156b6656c8
banwashere <banwashere@gmail.com>
2016-11-05 18:09:31 +0800

    Added filled Like button

--- Huatgram/HuatPostTableViewCell.swift
+++ Huatgram/HuatPostTableViewCell.swift
@@ -11,7 +11,7 @@ import KFSwiftImageLoader
 import Firebase
 
 class HuatPostTableViewCell: UITableViewCell {
-
+    
     @IBOutlet weak var huatImageView : UIImageView!
     @IBOutlet weak var likeBtn : UIButton!
     @IBOutlet weak var commentBtn : UIButton!
@@ -19,25 +19,37 @@ class HuatPostTableViewCell: UITableViewCell {
     @IBOutlet weak var likeCount: UILabel!
     
     var currentPost: HuatPost?
-
+    
     
     override func awakeFromNib() {
         super.awakeFromNib()
         // Initialization code
     }
-
+    
     override func setSelected(_ selected: Bool, animated: Bool) {
         super.setSelected(selected, animated: animated)
-
+        
         // Configure the view for the selected state
     }
     
     @IBAction func likeAction(_ sender: Any) {
         
         currentPost?.addLike()
+        
     }
     
     @IBAction func commentAction(_ sender: Any) {
+    }
+    
+    func checkLike(post : HuatPost) -> Bool{
+        
+        for userId in post.arrayOfLike{
+            
+            if userId == FIRAuth.auth()?.currentUser?.uid{
+                return true
+            }
+        }
+        return false
     }
     
     func configureCell(huatPost:HuatPost){
@@ -45,7 +57,13 @@ class HuatPostTableViewCell: UITableViewCell {
         huatImageView.loadImage(urlString: huatPost.imageUrl)
         commentLbl.text = huatPost.contentText
         likeCount.text = "\(huatPost.arrayOfLike.count) Likes"
-    
+        
+        if checkLike(post: huatPost) == true{
+            likeBtn.setImage(UIImage(named: "likeFilled"), for: .normal)
+        }
+        else {
+            likeBtn.setImage(UIImage(named: "like"), for: .normal)
+        }
     }
-
+    
 }
